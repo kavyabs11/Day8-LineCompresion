8b7be689632e5ff859afdb617f8986a965e37b71
kavyabs11 <kavyasomshekar11@gmail.com>
2022-09-14 08:19:46 -0700

    Equality

--- LineCmp/LineComp.cs
+++ LineCmp/LineComp.cs
@@ -19,8 +19,25 @@ namespace LineCmp
             Console.WriteLine("Enter the line point y2: ");
             int y2 = Convert.ToInt32(Console.ReadLine());
             var Length1 = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
-            Console.WriteLine(Length);
-
+            Console.WriteLine(Length1);
+            Console.WriteLine("Enter the line point x3: ");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the line point x4: ");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the line point y3: ");
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the line point y4: ");
+            int y4 = Convert.ToInt32(Console.ReadLine());
+            var Length2 = Math.Sqrt((Math.Pow(x3 - x4, 2) + Math.Pow(y3 - y4, 2)));
+            Console.WriteLine(Length2);
+            if (Length1.Equals(Length2))
+            {
+                Console.WriteLine("The two lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("The two lines are not equal");
+            }
         }
     }
 }
